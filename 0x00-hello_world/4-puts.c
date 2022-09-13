0ab3eb5fe69d9e84199fe269bfdd66f4a75fcca5
mileydm <davidmileym@gail.com>
2022-09-12 20:14:36 -0700

    Hello, puts done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,21 @@
-#include <studio>
+#include <stdio.h>
+
+
 
 /**
- * main - Printing with C
+ * main – Printing with C
  *
  * Return: Always 0 (Succeeded)
- * */
+*/
+
 int main(void)
+
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+
 }
+
+
